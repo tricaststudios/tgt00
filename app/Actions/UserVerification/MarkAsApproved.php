553ddb323e50905ef91a7d2a4b168c87562ae39a
DEV <tricaststudios@gmail.com>
2023-03-05 01:03:20 +0800

    fixed method

--- app/Actions/UserVerification/MarkAsApproved.php
+++ app/Actions/UserVerification/MarkAsApproved.php
@@ -4,11 +4,14 @@ namespace App\Actions\UserVerification;
 
 use App\Models\User;
 use App\Models\UserVerification;
+use Illuminate\Support\Facades\DB;
 
 class MarkAsApproved
 {
-    public function handle(UserVerification $user)
+    public function handle(UserVerification $verification)
     {
-        return $user->update(['status', 'approved', 'approved_at' => now()]);
+        return DB::transaction(function () use ($verification) {
+            $verification->update(['status', 'approved', 'approved_at' => now()]);
+        });
     }
 }
