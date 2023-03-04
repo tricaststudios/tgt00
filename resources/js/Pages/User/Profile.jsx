e0e2594f7f93e1c02f395644c7acc2cf8c5027c4
DEV <tricaststudios@gmail.com>
2023-03-05 00:56:12 +0800

    added verification statuses

--- resources/js/Pages/User/Profile.jsx
+++ resources/js/Pages/User/Profile.jsx
@@ -1,4 +1,5 @@
 import Section from '@/Components/AuthenticatedLayout/Section';
+import Alert from '@/Components/Common/Alert';
 import UpdateWalletAddressForm from '@/Components/Profile/UpdateWalletAddressForm';
 import VerificationForm from '@/Components/Profile/VerificationForm';
 import Authenticated from '@/Layouts/AuthenticatedLayout';
@@ -10,6 +11,8 @@ export default function Edit({ auth, mustVerifyEmail, status, verification }) {
         <Authenticated auth={auth}>
             <Head title="Profile" />
 
+            {verification?.status === 'approved' && <Alert type="success" title="Account is fully verified." />}
+
             <Section>
                 <UpdateProfileInformationForm mustVerifyEmail={mustVerifyEmail} status={status} className="max-w-xl" />
             </Section>
@@ -18,9 +21,13 @@ export default function Edit({ auth, mustVerifyEmail, status, verification }) {
                 <UpdateWalletAddressForm mustVerifyEmail={mustVerifyEmail} status={status} className="max-w-xl" />
             </Section>
 
-            <Section>
-                <VerificationForm status={status} verification={verification} className="max-w-xl" />
-            </Section>
+            {verification?.status === 'pending' && <Alert type="warning" title="Verification is ongoing." />}
+            {verification?.status === 'declined' && <Alert type="warning" title="Verification failed." message={verification.remarks} />}
+            {verification?.status !== 'approved' && (
+                <Section>
+                    <VerificationForm status={status} verification={verification} className="max-w-xl" />
+                </Section>
+            )}
         </Authenticated>
     );
 }
