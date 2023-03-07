dc43d0cb0c60eb9211a6139c103089e73e203edc
DEV <tricaststudios@gmail.com>
2023-03-07 10:44:49 +0800

    added dashboard blogs

--- resources/js/Pages/User/Dashboard.jsx
+++ resources/js/Pages/User/Dashboard.jsx
@@ -1,16 +1,49 @@
 import P from '@/Components/AuthenticatedLayout/P';
 import Section from '@/Components/AuthenticatedLayout/Section';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { CurrencyDollarIcon } from '@heroicons/react/24/solid';
 import { Head } from '@inertiajs/react';
+
+const posts = [
+    { title: 'Boost your conversion rate', href: 'https://cointelegraph.com/tags/bitcoin', imageUrl: '/images/dashboard/crypto.jpg' },
+    {
+        title: 'Benefits of Holding Stocks for the Long-Term',
+        href: 'https://www.investopedia.com/articles/investing/052216/4-benefits-holding-stocks-long-term.asp',
+        imageUrl: '/images/dashboard/investment.jpeg',
+    },
+    // More posts...
+];
 
 export default function Dashboard({ auth }) {
     return (
         <AuthenticatedLayout auth={auth}>
             <Head title="Dashboard" />
-            <Section>
-                <P>Welcome!</P>
-            </Section>
+            <div className="mx-auto grid max-w-2xl grid-cols-1 gap-y-20 gap-x-8 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+                {posts.map((item, index) => (
+                    <Section>
+                        <a key={index} href={item.href} target="_blank">
+                            <span className="flex flex-col items-start justify-between">
+                                <div className="relative w-full">
+                                    <img
+                                        src={item.imageUrl}
+                                        alt=""
+                                        className="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
+                                    />
+                                    <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
+                                </div>
+                                <div className="max-w-xl">
+                                    <div className="group relative">
+                                        <P className="mt-8 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                                            <span className="absolute inset-0" />
+                                            {item.title}
+                                        </P>
+                                        {/* <p className="line-clamp-3 mt-5 text-sm leading-6 text-gray-600">{post.description}</p> */}
+                                    </div>
+                                </div>
+                            </span>
+                        </a>
+                    </Section>
+                ))}
+            </div>
         </AuthenticatedLayout>
     );
 }
